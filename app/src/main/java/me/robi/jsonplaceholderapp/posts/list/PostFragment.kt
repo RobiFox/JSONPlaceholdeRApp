bb741446de9bd89b7768696d26285310418b5e50
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-20 18:50:39 +0100

    removed todos

--- app/src/main/java/me/robi/jsonplaceholderapp/posts/list/PostFragment.kt
+++ app/src/main/java/me/robi/jsonplaceholderapp/posts/list/PostFragment.kt
@@ -67,10 +67,8 @@ class PostFragment : JsonFragment() {
 
     companion object {
 
-        // TODO: Customize parameter argument names
         const val ARG_COLUMN_COUNT = "column-count"
 
-        // TODO: Customize parameter initialization
         @JvmStatic
         fun newInstance(columnCount: Int) =
             PostFragment().apply {
